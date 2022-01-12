e3f25fc9dbe0e7126d864e041c05eaff3c213c45
Valeria Makarevich <valeriamakarevich@outlook.com>
2022-01-12 18:22:49 +0300

    Сделала два задания

---
+++ Экзамен_1_1/Экзамен_1_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    int max[10];
+
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите 10 элементов массива" << std::endl;
+
+    for (int a = 0; a < 10; a++) {
+
+        std::cin >> max[a];
+    }
+
+    std::cout << "Введенный массив" << std::endl;
+
+    for (int a = 0; a < 10; a++) {
+
+        std::cout << "[" << a << "]: " << max[a] << std::endl;
+        return 0;
+    }
+}
---
+++ Экзамен_1_2/Экзамен_1_2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main() {
+
+    int add[10];
+    std::cout << "Массив случайных чисел от 1 до 100" << std::endl;
+
+    for (int a = 0; a < 10; a++) {
+        add[a] = rand() % 100 + 1;
+        std::cout << "[" << a << "]: " << add[a] << std::endl;
+
+    }
+    int max_v = 0;
+
+    for (int a = 0; a < 10; a++) {
+        if (add[a] > max_v) {
+            max_v = add[a];
+        }
+    }
+    std::cout << "Максимальное значение:" << max_v << std::endl;
+    int min_v = 1;
+
+    for (int a = 0; a < 10; a++) {
+        if (add[a] < min_v) {
+            min_v = add[a];
+        }
+    }
+    std::cout << "Минимальное  значение:" << min_v << std::endl;
+    int sum = 0;
+    for (int a = 0; a < 10; a++) {
+        sum += add[a];
+    }
+    sum = sum / 10;
+    std::cout << sum;
+
+}
